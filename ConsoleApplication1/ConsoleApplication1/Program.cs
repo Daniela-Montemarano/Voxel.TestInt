218c393a997c94b1a259bb5237646afabda12f8e
Daniela Montemarano <dmontemarano@voxelgroup.net>
2018-06-13 09:08:13 +0200

    commit desde develop

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo desde develop1");
+            Console.WriteLine("Hola mundo desde develop1.");
         }
     }
 }
